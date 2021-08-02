53931ec7b1ab626fa5e068c5eab13726697e3fa5
bunnie26 <66902288+bunnie26@users.noreply.github.com>
2021-08-02 15:28:13 +0530

    Updates

---
+++ precache-manifest.b07535cbad864df768b123f9dfb57079.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7d361df9046aa12193806895955b8bf",
+    "url": "/2048-Game/index.html"
+  },
+  {
+    "revision": "cf21875ae09cc310bc37",
+    "url": "/2048-Game/static/css/main.d8cf65f4.chunk.css"
+  },
+  {
+    "revision": "781a3480e8042bbe23df",
+    "url": "/2048-Game/static/js/2.01cc639e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/2048-Game/static/js/2.01cc639e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf21875ae09cc310bc37",
+    "url": "/2048-Game/static/js/main.8114017f.chunk.js"
+  },
+  {
+    "revision": "1f331b5b65612f928579",
+    "url": "/2048-Game/static/js/runtime-main.c0c088d5.js"
+  }
+]);
